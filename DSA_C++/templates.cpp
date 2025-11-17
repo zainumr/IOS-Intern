4ba5cbae58a2383e3ab170bc3ba3d80571c2339c
Zain Umer <zainumer692@gmail.com>
2025-11-18 00:58:43 +0500

    change file order

--- DSA_C++/templates.cpp
+++
@@ -1,9 +0,0 @@
-// ================================================
-/*
-  Task 2:  Number Classification (positive/negative, even/odd & prime/non-prime)
-
-  Pesudocode:
-  1. 
-  
-  // Code: 
-*/
