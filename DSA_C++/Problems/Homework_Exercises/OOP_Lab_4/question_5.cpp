f00b37688be659d9a014ca67ced143f9a7b7a6e5
Zain Umer <zainumer692@gmail.com>
2025-12-12 19:00:46 +0500

    completed OOP lab 4 assignment

---
+++ DSA_C++/Problems/Homework_Exercises/OOP_Lab_4/question_5.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+using namespace std;
+
+// 1
+struct Students {
+    int rollNo;
+    char name[30];
+    float marks;
+
+    // 2
+    void setData(int r, const char n[], float m)
+    {   
+        rollNo = r;
+        strcpy(name, n);   // copy char array into member
+        marks = m;
+        
+    }
+
+    void display()
+    {
+        cout<< "Roll No: " << rollNo << '\n';
+        cout<< "Name: " << name << '\n';
+        cout<< "Marks: " << marks << '\n';
+        cout << '\n';
+    }
+
+
+
+};
+
+int main()
+{
+/*
+    ================= Problem =================
+    1. Define a structure Student with the following members:
+        . int rollNo
+        . char name[30]
+        . float marks
+    2. Add member functions:
+        . setData(int r, const char n[], float m) → to assign values to the data members.
+        . display() → to print student details.
+    3. In main():
+        . Create 3 student objects.
+    4. Use setData() to assign values.
+    5. Display all student details using display().
+*/
+    
+    // 3
+    Students s1, s2, s3;
+
+    // 4
+    s1.setData(1, "hi", 85.5);
+    s2.setData(2, "Sara", 91);
+    s3.setData(3, "Hamza", 77);
+
+    // 5
+    s1.display();
+    s2.display();
+    s3.display();
+    
+    cout<< "Total Students Created = " << 3 << '\n';
+    
+    return 0;
+}
